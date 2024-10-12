f774d858e65c13f1660af74f10e26ad21b95e853
ugonner <umeokwonnabonaventure@gmail.com>
2024-10-12 14:14:18 +0100

    resolves lint by removing export statement

--- src/app/app/(root)/events/check-in-form/page.tsx
+++ src/app/app/(root)/events/check-in-form/page.tsx
@@ -1,10 +1,11 @@
 import { EventCheckInForm } from "../../../../../components/features/events/EventCheckInForm"
+import GridContainer from '@/components/ui/containers/GridContainer'
 
-export const EventCheckInFormPage = () => {
+const EventCheckInFormPage = () => {
     return (
-        <>
+        <GridContainer>
             <EventCheckInForm />
-        </>
+        </GridContainer>
     )
 }
 export default EventCheckInFormPage;
--- src/app/app/(root)/events/checkin/page.tsx
+++ src/app/app/(root)/events/checkin/page.tsx
@@ -2,12 +2,6 @@
 
 import EventCard from '@/components/features/events/EventCard'
 import GridContainer from '@/components/ui/containers/GridContainer'
-import { Skeleton } from '@/components/ui/skeleton'
-import { useToast } from '@/components/ui/use-toast'
-import EVENTS from '@/consts/Events'
-import useApi from '@/hooks/useApi'
-import { getToken } from '@/store/reducers/authSlice'
-import { Event, IApiResponse } from '@/types/index'
 import { useParams, useRouter, useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
